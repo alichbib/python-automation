0ea17b55250583d5b9fe03c7bd6aa788dca4fe47
ChbibAli <119846225+ChbibAli@users.noreply.github.com>
2022-12-08 10:37:28 +0200

    Whatsapp-automation

---
+++ whatsap-automation.py
@@ -0,0 +1,27 @@
+# import pywhatkit
+
+# #sending a specific msg to a number in a specific time
+# phone_number = input("enter phone number: ")
+# pywhatkit.sendwhatmsg(phone_number, "test", 18, 35, )
+
+# #sending a message to a group
+# group_id = 22
+# pywhatkit.sendwhatmsg_to_group("write id", "test groupt", 18, 59)
+data = { "ali" : {
+      "gym": 100,
+      "eng":95,
+      "math":80,
+      "programming":100
+},
+"samer" : {
+    "gym": 45,
+      "eng":100,
+      "math":92,
+      "programming":20
+}
+}
+print(data['ali'].keys())
+
+
+
+
